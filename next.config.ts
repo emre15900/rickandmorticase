528061020685718f95ae881e6e2de848e89cd399
Emre Sayın <emresayinweb@gmail.com>
2025-08-13 23:34:25 +0300

    Fix Next.js configuration for shadcn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['rickandmortyapi.com'],
+  },
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
